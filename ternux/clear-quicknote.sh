a8731db8146b12460c2d25001e3a61b7e8d344f5
Christopher Carroll <ccarroll4812@gmail.com>
2023-07-10 22:43:03 -0400

    added termux stuff

---
+++ ternux/clear-quicknote.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This script was desinged to be used with the application Markor, so I could have a note app that will not interfere w/ my ADHD
+
+source_path=~/Lab/notebook/quicknote.md
+target_directory=~/Lab/notebook/archived-quicknotes/
+
+# Get the current date
+formatted_date=$(date "+%Y-%m-%d")
+target_filename="${formatted_date}-quicknote.md"
+target_path="${target_directory}/${target_filename}"
+
+# Copy the contents of the source file to the target file
+cp "$source_path" "$target_path"
+
+# Delete the source file
+rm "$source_path"
+
+# Create a new empty source file
+touch "$source_path"
