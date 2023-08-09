8cb46d7cbc36841d627603ee1a51481eabcc1a09
Christopher Carroll <cmcarroll@scmpeoc3t.army.mil>
2023-08-09 14:51:30 -0400

    Added in the assistant and log in clojure

--- sapphire/src/sapphire/gpt.clj
+++ sapphire/src/sapphire/gpt.clj
@@ -1,16 +1,22 @@
 (ns sapphire.gpt)
 (require '[wkok.openai-clojure.api :as api])
 
-(def empty-chat [{:role "system" :content "You are a helpful assistant named Sapphire.EXE. your purpose is to help me manage my schedule, projects, and ADHD"}
-                            {:role "assistant" :content "What is your name and purpose?"}])
+(def empty-chat [{:role "system" :content "You are a helpful assistant named Sapphire.EXE. your purpose is to help me manage my schedule, projects, and ADHD"}])
 
 (defn add-new-message [message]
   (let [new-map {:role "user" :content message}]
         (conj empty-chat new-map)))
 
 (defn chat-with-assistant [message]
-  (println (get-in (api/create-chat-completion {:model "gpt-3.5-turbo"
-                                                :messages (add-new-message message)})[:choices 0 :message :content])))
+  (get-in (api/create-chat-completion {:model "gpt-3.5-turbo"
+                                                :messages (add-new-message message)})[:choices 0 :message :content]))
 
-(chat-with-assistant "How are you today?")
+;(chat-with-assistant "How are you today?")
 
+(defn perpetual-loop []
+  (loop []
+    (let [input (read-line)]
+        (println (chat-with-assistant input))
+    (recur))))
+
+(perpetual-loop)
